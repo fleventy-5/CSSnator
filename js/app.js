81f2945bf4434589da238d342e1496f54422a715
fleventy-5 <you@example.com>
2021-03-05 21:00:14 +0530

    Testing

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
-
-
-let btn = document.querySelector('.dropbtn')
-btn.onclick = function(){
-	document.querySelector('.dropdown-content').classList.add("show")
-}
-window.onclick = function(e){
-	if(!e.target.matches('.dropbtn')){
-		document.querySelector('.dropdown-content').classList.remove("show")	
-	}
-}
+let sel = document.getElementById('opt');
+sel.addEventListener('change',e=>{
+		alert(e.target.value);
+	});
