c218032cd75aa442b169508bb993264808425c8b
fleventy-5 <templyngkhoi@gmail.com>
2021-03-05 21:46:05 +0530

    another update

--- js/border-rad.js
+++ js/border-rad.js
@@ -1,4 +1,4 @@
 export function borderRad(){
-	document.querySelector('.input-container').insertAdjacentHTML("after",
+	document.querySelector('.input-container').insertAdjacentHTML("afterend",
 		"<p><input type='text' class = 'input' id = 'top-l' placeholder='Top left corner'><input type='text' class = 'input' id = 'top-r' placeholder='Top right corner'><input type='text' class = 'input' id = 'bot-l' placeholder='Bottom left corner'><input type='text' class = 'input' id = 'bot-r' placeholder='Bottom right corner'></p>");
 }
