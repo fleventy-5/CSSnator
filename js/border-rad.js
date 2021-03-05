04d4ce01c0f89b78cf3b6c1d82f5c22e251b154b
fleventy-5 <templyngkhoi@gmail.com>
2021-03-05 21:39:35 +0530

    Test

--- js/border-rad.js
+++ js/border-rad.js
@@ -1,3 +1,8 @@
 export function borderRad(){
-	document.querySelector('.input-container')
+	document.querySelector('.input-container').insertAdjacentHTML("after",
+		"<p><input type='text' class = 'input' id = 'top-l' placeholder='Top left corner'>	
+		<input type='text' class = 'input' id = 'top-r' placeholder='Top right corner'>
+			<input type='text' class = 'input' id = 'bot-l' placeholder='Bottom left corner'>
+			<input type='text' class = 'input' id = 'bot-r' placeholder='Bottom right corner'>
+		</p>"" )
 }
