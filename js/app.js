076b013113fb03fd4df9580b86e24955080deb91
fleventy-5 <templyngkhoi@gmail.com>
2021-03-13 17:22:14 +0530

    Error fixing

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ import  * as BRad from './border-rad.js'
 let sel = document.getElementById('opt');
 sel.addEventListener('change',e=>{
 		if(e.target.value == "brRad"){
-			document.getElementById('one').remove();
-			document.getElementById('two').remove();
+			//document.getElementById('one').remove();
+			//document.getElementById('two').remove();
 			BRad.borderRad();
 		}
 	});
