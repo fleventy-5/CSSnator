3838e3d139d1339733189203a73a397e705ecd88
fleventy-5 <templyngkhoi@gmail.com>
2021-03-13 17:25:28 +0530

    Error fixing

--- js/app.js
+++ js/app.js
@@ -3,8 +3,13 @@ import  * as BRad from './border-rad.js'
 let sel = document.getElementById('opt');
 sel.addEventListener('change',e=>{
 		if(e.target.value == "brRad"){
-			//document.getElementById('one').remove();
-			//document.getElementById('two').remove();
-			BRad.borderRad();
+			if(document.getElementById('one')){
+				document.getElementById('one').remove();
+				document.getElementById('two').remove();
+				BRad.borderRad();
+			} else {
+				BRad.borderRad();
+			}
+			
 		}
 	});
